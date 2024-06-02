e25aa53217348550d430f8a3d66b2de52c82817a
senpaiblue <singhsakcham29@email.com>
2024-06-02 05:33:33 +0530

    bento-grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
@@ -9,6 +11,8 @@ export default function Home() {
     <div className="max-w-7xl w-full">
       <FloatingNav navItems={[{name: "Home", link: "/", icon: <FaHome/>}]}/>
       <Hero/>
+      <Grid/>
+      <RecentProjects/>
     </div>
    </main>
   );
---
+++ components/RecentProjects.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecentProjects = () => {
+  return (
+    <div className='py-20'>
+      
+    </div>
+  )
+}
+
+export default RecentProjects
